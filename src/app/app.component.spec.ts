34244570835df5f150b9f1230d763cb06878bf0e
Pawan Kuamr Ranjan <kumarpawanranjan000@gmail.com>
2025-06-23 11:36:03 +0530

    project name updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pdf-esignature'`, () => {
+  it(`should have as title 'doc-e-sign'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pdf-esignature');
+    expect(app.title).toEqual('doc-e-sign');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pdf-esignature');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, doc-e-sign');
   });
 });
